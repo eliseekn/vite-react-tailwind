2d47d1624446c0afe5cb75d613bc0585dc3b6156
eliseekn <eliseekn@gmail.com>
2021-09-08 14:17:18 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+    return (
+        <div className="container mx-auto p-5 flex justify-center">
+            <p className="text-lg">Hello Vite + React + Tailwind!</p>
+        </div>
+    )
+}
+
+export default App
